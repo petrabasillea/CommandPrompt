c50a1ec593cab2848599e4dc624a3a8f387d51b3
petrabasillea <petra.basillea@gmail.com>
2022-08-28 15:36:58 +0700

    push_153658

---
+++ git_push.bat
@@ -0,0 +1,25 @@
+echo off
+set CUR_YYYY=%date:~10,4%
+set CUR_MM=%date:~4,2%
+set CUR_DD=%date:~7,2%
+set CUR_HH=%time:~0,2%
+if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
+
+set CUR_NN=%time:~3,2%
+set CUR_SS=%time:~6,2%
+set CUR_MS=%time:~9,2%
+
+::manual Var
+::set CUR_MM="01"
+::set CUR_DD="19"
+set commit="push_"%CUR_HH%%CUR_NN%%CUR_SS%
+set branch="master"
+
+git config --global "user.email "petrabasillea@gmail.com"
+git config --global "user.name "petrabasillea"
+
+git status
+git add .
+git commit -m "%commit%"
+::git commit --amend --no-edit --date="%CUR_YYYY%-%CUR_MM%-%CUR_DD% %CUR_HH%:%CUR_NN%:%CUR_SS% -0700"
+git push origin %branch%
